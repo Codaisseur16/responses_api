e1fe8063796847233f4d255c958eb2ac1e6afd78
cicomsa <ioanacomsa85@gmail.com>
2018-05-04 11:30:34 +0200

    connection to the users table

--- src/responses/controller.ts
+++ src/responses/controller.ts
@@ -2,38 +2,44 @@ import {JsonController, Get, Post, HttpCode, Param, Body } from 'routing-control
 import Responses from './entity'
 import * as request from 'superagent'
 
+const checkrole = async (user) => {
+  const res = await request
+    .post('http://users:4003/teacher/')
+    .send({id: user.user_id})
+    console.log(res.body)
+    return res.body
+}
+
 @JsonController()
 export default class ResponsesController {
 
-@Post('/responses')
-@HttpCode(201)
-async createResponse(
-    @Body() response: Responses
-) {
-     await request
-    .post('http://users:4003/teacher/')
-    .send({id:Number(response.user_id)})
-    .then(response2 => {
-      const teacher = response2.body.user.teacher
-      if(teacher === true || teacher === null) {
-        throw new Error("User not allowed to save test results")
-      } else {
-        const responsetosend = {
-        user_id: response.user_id,
-        teacher: response.teacher,
-        quiz_id: response.quiz_id,
-        score: response.score
-    }
+  @Post('/responses')
+  @HttpCode(201)
+  async createResponse(
+      @Body() response: Responses
+  ) {
+        const role = await checkrole(response)
+        console.log(role, response)
+        const teacher = role.teacher
+        let responseToSend
+        if(teacher === true || teacher === null) {
+          throw new Error("User not allowed to save test results")
+        } else {
+            responseToSend = {
+              user_id: response.user_id,
+              teacher: response.teacher,
+              quiz_id: response.quiz_id,
+              score: response.score
+            }
+          }
 
-    return request
-    .post('http://webhooks:4004/postquizresult/')
-    .send({qobject: responsetosend})
-    .then(response2=> {
-        response.save()
-        return response2.text})
-      }
-    })
-  }
+        return request
+        .post('http://webhooks:4004/postquizresult/')
+        .send({qobject: responseToSend})
+        .then(response2=> {
+            response.save()
+            return response2.text})
+    }
 
 
 @Get('/responses/user/:user_id')
@@ -78,14 +84,6 @@ getAnswer(
 ){
     return Responses.findOneById(id)
 }
-
-// @Post('/logins')
-// @HttpCode(201)
-// createScore(
-//     @Body() answer:{email,password}
-// ) {
-//     return answer
-// }
 
 }
 
